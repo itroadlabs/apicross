8d4d9dfd2d8a17efb9f5633fd408b6a918ec702e
VictorKrapivin <victor-vk@yandex.ru>
2021-07-27 23:24:16 +0300

    support Spring Cloud OpenFeign client code generation / add toString()

--- apicross-spring-cloud-openfeign/src/main/java/io/github/itroadlabs/apicross/springcloudopenfeign/SpringCloudOpenFeignCodeGeneratorOptions.java
+++ apicross-spring-cloud-openfeign/src/main/java/io/github/itroadlabs/apicross/springcloudopenfeign/SpringCloudOpenFeignCodeGeneratorOptions.java
@@ -3,11 +3,13 @@ package io.github.itroadlabs.apicross.springcloudopenfeign;
 import io.github.itroadlabs.apicross.java.JavaCodeGeneratorOptions;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
+@ToString(callSuper = true)
 public class SpringCloudOpenFeignCodeGeneratorOptions extends JavaCodeGeneratorOptions {
     private boolean enableApicrossJavaBeanValidationSupport = false;
     private boolean useQueryStringParametersObject = true;
