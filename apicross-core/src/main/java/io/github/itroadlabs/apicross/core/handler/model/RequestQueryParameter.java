937126745e234e6b65efefbc262d15ded4a36ca7
VictorKrapivin <victor-vk@yandex.ru>
2021-10-13 18:47:02 +0300

    add parameter content encoding feature

--- apicross-core/src/main/java/io/github/itroadlabs/apicross/core/handler/model/RequestQueryParameter.java
+++ apicross-core/src/main/java/io/github/itroadlabs/apicross/core/handler/model/RequestQueryParameter.java
@@ -4,7 +4,18 @@ import io.github.itroadlabs.apicross.core.NamedDatum;
 import io.github.itroadlabs.apicross.core.data.model.DataModel;
 
 public class RequestQueryParameter extends NamedDatum {
+    private final boolean jsonEncoding;
+
     public RequestQueryParameter(String name, String resolvedName, String description, DataModel type, boolean required, boolean deprecated) {
+        this(name, resolvedName, description, type, required, deprecated, false);
+    }
+
+    public RequestQueryParameter(String name, String resolvedName, String description, DataModel type, boolean required, boolean deprecated, boolean jsonEncoding) {
         super(name, resolvedName, description, type, required, deprecated);
+        this.jsonEncoding = jsonEncoding;
+    }
+
+    public boolean isJsonEncoding() {
+        return jsonEncoding;
     }
 }
